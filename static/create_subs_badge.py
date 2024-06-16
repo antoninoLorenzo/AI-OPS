41b8ddbecbee5443711b8536f3fe3f607373a79c
Antonino Lorenzo <94693967+antoninoLorenzo@users.noreply.github.com>
2024-06-16 15:16:40 +0200

    PyLint workflow

---
+++ static/create_subs_badge.py
@@ -0,0 +1,29 @@
+"""
+https://github.com/Silleellie/pylint-github-action
+"""
+import os.path
+import re
+import sys
+
+README_PATH = sys.argv[1]
+NUMERIC_SCORE = sys.argv[2]
+BADGE_TEXT = sys.argv[3]
+BADGE_COLOR = sys.argv[4]
+
+if not os.path.isfile(README_PATH):
+    raise FileNotFoundError(f"README.md path is wrong, no file can be located at {README_PATH}")
+
+with open(README_PATH, "r", encoding="utf8") as f:
+    content = f.read()
+
+query = f"{BADGE_TEXT}-{NUMERIC_SCORE}-{BADGE_COLOR}?logo=python&logoColor=white"
+badge_url = f"https://img.shields.io/badge/{query}"
+
+patt = r"(?<=!\[pylint]\()(.*?)(?=\))"
+if re.search(patt, content) is None:
+    raise ValueError("Pylint badge not found! Be sure to put an empty one which acts as a placeholder "
+                     "if this is your first run. Check README.md for examples!")
+
+result = re.sub(patt, badge_url, content)
+with open(README_PATH, "w", encoding="utf8") as f:
+    f.write(result)
---
+++ static/get_score_color.py
@@ -0,0 +1,26 @@
+"""
+https://github.com/Silleellie/pylint-github-action
+"""
+import re
+import os
+import sys
+
+COLOR_BAD_SCORE = str(sys.argv[1])
+COLOR_OK_SCORE = str(sys.argv[2])
+COLOR_GOOD_SCORE = str(sys.argv[3])
+COLOR_PERFECT_SCORE = str(sys.argv[4])
+
+with open("pylint_score.txt", "r") as f:
+    pylint_result = f.read()
+numeric_score = re.search(r"(?<=\s)(\d+\.\d+)\/\d+(?=\s)", pylint_result).group().split("/")[0]
+
+color = COLOR_BAD_SCORE
+if 5 <= float(numeric_score) < 8:
+    color = COLOR_OK_SCORE
+elif 8 <= float(numeric_score) < 10:
+    color = COLOR_GOOD_SCORE
+elif float(numeric_score) == 10:
+    color = COLOR_PERFECT_SCORE
+
+os.system("echo badge_color=" + color + " >> $GITHUB_OUTPUT")
+os.system("echo pylint_score=" + numeric_score + " >> $GITHUB_OUTPUT")
