e1dc2065861bab0201013d21f5e8d376880b9650
Antonino Lorenzo <94693967+antoninoLorenzo@users.noreply.github.com>
2024-06-19 11:37:05 +0200

    Refactored RAG Evaluation Code from Jupyter to evaluation.py and metrics.py

--- test/benchmarks/rag/evaluation.py
+++ test/benchmarks/rag/evaluation.py
@@ -3,11 +3,35 @@ Gets executed by GitHub Actions, loads the synthetic dataset generated in `datas
 into the Qdrant vector database, performs evaluation of the RAG pipeline and outputs the results
 as plots in `rag_evaluation_out`; the plots are then added to the relevant EVALUATION.md file.
 """
+import os
 import json
+import textwrap
 from pathlib import Path
 from tqdm import tqdm
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from dotenv import load_dotenv
+
 from src.agent.llm import LLM
 from src.agent.knowledge import Store, Collection, Document, Topic
+from test.benchmarks.rag.metrics import HuggingFaceLLM, ContextRecall, ContextPrecision, EVAL_PROMPTS
+
+
+GEN_PROMPT = {
+    'gemma:2b': {
+        'sys': textwrap.dedent("""
+            You are a Cybersecurity professional assistant, your job is to provide an answer to context specific questions.
+            You will be provided with additional Context information to provide an answer.
+        """),
+        'usr': textwrap.dedent("""
+            Question: {query}
+            Context:
+            {context}
+        """)
+    }
+}
 
 
 def init_knowledge_base(data: dict[str: list[Topic]]) -> Store:
@@ -47,7 +71,47 @@ def init_knowledge_base(data: dict[str: list[Topic]]) -> Store:
     return store
 
 
-def evaluate(vdb: Store, synthetic_qa_path: str):
+def generate_evaluation_dataset(vdb: Store, qa_paths: list, model: str = 'gemma:2b'):
+    """Uses the RAG pipeline to generate an evaluation dataset composed of
+    questions and ground truths from Q&A dataset and context + answers from
+    the RAG pipeline."""
+    qa = pd.concat(
+        [pd.read_json(path) for path in qa_paths],
+        ignore_index=True
+    )
+
+    def gen_context_answer(question: str, llm: LLM):
+        points = vdb.retrieve(question, 'owasp')
+        context_list = [f'{p.payload["title"]}: {p.payload["text"]}' for p in points]
+        context = '\n'.join(context_list)
+        answer = llm.query(
+            messages=[
+                {'role': 'system', 'content': GEN_PROMPT[model]['sys']},
+                {'role': 'user', 'content': GEN_PROMPT[model]['usr'].format(query=question, context=context)}
+            ],
+            stream=False
+        )['message']['content']
+
+        return context_list, answer
+
+    generator = LLM(model=model)
+    eval_data = []
+    for i, items in tqdm(qa.iterrows(), total=len(qa), desc='Retrieving context and generating answers.'):
+        ctx, ans = gen_context_answer(items.question, generator)
+        eval_data.append({
+            'contexts': ctx,
+            'question': items.question,
+            'answer': ans,
+            'ground_truth': items.ground_truth
+        })
+    return pd.DataFrame(eval_data)
+
+
+def evaluate(vdb: Store, qa_paths: list,
+             evaluation_api_key: str,
+             generation_model: str = 'gemma:2b',
+             evaluation_model: str = 'mistral:7b',
+             eval_hf_url: str = "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.3"):
     """Given the Vector Database and the synthetic Q&A dataset
     generated in `dataset_generation.ipynb` runs the evaluation
     process for the RAG pipeline.
@@ -58,10 +122,100 @@ def evaluate(vdb: Store, synthetic_qa_path: str):
 
     - Evaluating the full contexts-question-answer-ground_truths dataset.
     """
-    pass
+    eval_dataset = generate_evaluation_dataset(vdb, qa_paths, generation_model)
+
+    # Setup evaluation metrics
+    hf_llm = HuggingFaceLLM(eval_hf_url, evaluation_api_key)
+    ctx_recall = ContextRecall(
+        EVAL_PROMPTS[evaluation_model]['context_recall']['sys'],
+        EVAL_PROMPTS[evaluation_model]['context_recall']['usr'],
+        hf_llm
+    )
+    ctx_precision = ContextPrecision(
+        EVAL_PROMPTS[evaluation_model]['context_precision']['sys'],
+        EVAL_PROMPTS[evaluation_model]['context_precision']['usr'],
+        hf_llm
+    )
+
+    # Run
+    recall = []
+    for i, item in tqdm(eval_dataset.iterrows(), total=len(eval_dataset), desc='Measuring Context Recall'):
+        ctx = '\n\n'.join(item.contexts)
+        ans = item.answer
+        recall.append(ctx_recall.compute(ans, ctx))
+
+    precision = []
+    for i, item in tqdm(eval_dataset.iterrows(), total=len(eval_dataset), desc='Measuring Context Recall'):
+        qst = item.question
+        ctx = '\n\n'.join(item.contexts)
+        ans = item.answer
+        precision.append(ctx_precision.compute(qst, ans, ctx))
+
+    return pd.DataFrame({
+        'context_recall': recall,
+        'context_precision': precision
+    })
+
+
+def update_evaluation_plots(results_df: pd.DataFrame):
+    # Load the evaluation history
+    with open('../../../data/rag_eval/results/results.json', 'r+', encoding='utf-8') as fp:
+        content: list = json.load(fp)
+        res = results_df.mean()
+        content.append({'context_precision': res.context_precision, 'context_recall': res.context_recall})
+        fp.seek(0)
+        json.dump(content, fp, indent=4)
+    history = pd.DataFrame(content)
+
+    def plot_eval(plot_df: pd.DataFrame, name: str):
+        """Create a plot for an evaluation metric, the columns should be named 'x' and 'y'"""
+        sns.lineplot(data=plot_df, x='x', y='y', zorder=0)
+        plt.scatter(
+            plot_df.iloc[1:]['x'],
+            plot_df.iloc[1:]['y'],
+            color='#000000',
+            s=15,
+            zorder=1
+        )
+
+        plt.ylim(0, 1)
+        plt.xticks(range(0, len(plot_df)))
+
+        plt.title(f'RAG Evaluation: {name}')
+        plt.ylabel(name)
+        plt.xlabel('')
+        return plt
+
+    # Output the updated evaluation plots
+    plots = {}
+    for col in history.columns:
+        values = history[col].to_list()
+        plots[col] = [{'x': i, 'y': val} for i, val in enumerate(values)]
+
+    ctx_precision_df = pd.DataFrame(plots['context_precision'])
+    ctx_recall_df = pd.DataFrame(plots['context_recall'])
+
+    plt_ctx_precision = plot_eval(ctx_precision_df, 'Context Precision')
+    plt_ctx_precision.savefig('../../../data/rag_eval/results/plots/context_precision.png')
+
+    plt_ctx_recall = plot_eval(ctx_recall_df, 'Context Recall')
+    plt_ctx_recall.savefig('../../../data/rag_eval/results/plots/context_recall.png')
 
 
 if __name__ == '__main__':
+    load_dotenv()
+    hf_api_key = os.environ.get('HF_API_KEY')
+    if not hf_api_key:
+        raise RuntimeError('Missing HuggingFace API Key in .env')
+
     knowledge_base: Store = init_knowledge_base({
         '../../../data/json/owasp.json': [Topic.WebPenetrationTesting]
     })
+
+    synthetic_qa_paths = [
+        '../../../data/rag_eval/owasp_100.json',
+        # '../../../data/rag_eval/owasp_100-200.json'
+    ]
+
+    eval_results_df = evaluate(knowledge_base, synthetic_qa_paths, hf_api_key)
+    update_evaluation_plots(eval_results_df)
--- test/benchmarks/rag/metrics.py
+++ test/benchmarks/rag/metrics.py
@@ -11,17 +11,66 @@ import numpy as np
 
 
 EVAL_PROMPTS = {
-    'mistral': {
+    'mistral:7b': {
         'context_recall': {
-            'sys': '',
-            'usr': ''
+            'sys': textwrap.dedent("""
+                Given a context, and an answer, analyze each sentence in the answer and classify if the sentence can be attributed to the given context or not. Use only "Yes" (1) or "No" (0) as a binary classification. 
+                
+                Your output should contain a list of 0 or 1 for each sentence, also it should be a JSON string as follows:
+                {{"result": [1, 0, ...]}}
+                
+                IMPORTANT:
+                - Only provide the JSON string in the specified format. Do not include any additional text.
+                - If the answer mentions that available information wasn't sufficient, your response should be the following: {{"result": [0]}}
+            """),
+            'usr': textwrap.dedent("""
+                Answer:
+                {answer}
+                
+                Context:
+                {context}
+                
+                Your output should contain a list of 0 or 1 for each sentence, also it should be a JSON string as follows:
+                {{"result": [1, 0, ...]}}
+                
+                IMPORTANT:
+                - Only provide the JSON string in the specified format. Do not include any additional text.
+                - If the answer mentions that available information wasn't sufficient, your response should be the following: {{"result": [0]}}
+            """)
         },
         'context_precision': {
-            'sys': '',
-            'usr': ''
+            'sys': textwrap.dedent("""
+                Given question, answer and context verify if the context was useful in arriving at the given answer. 
+                Use only "Useful" (1) or "Not Useful" (0) as a binary classification. 
+                
+                Your output should contain a list of 0 or 1 for each sentence, also it should be a JSON string as follows:
+                {{"result": [1, 0, ...]}}
+                
+                IMPORTANT:
+                - Only provide the JSON string in the specified format. Do not include explanations or any additional text.
+                - If the answer do not provide a response to the question or mentions that available information wasn't sufficient, your response should be the following:  {{"result": [0]}}
+            """),
+            'usr': textwrap.dedent("""
+                Question:
+                {question}
+                
+                Context:
+                {context}
+                
+                Answer:
+                {answer}
+                
+                Your output should contain a list of 0 or 1 for each sentence, also it should be a JSON string as follows:
+                {{"result": [1, 0, ...]}}
+                
+                IMPORTANT:
+                - Only provide the JSON string in the specified format. Do not include explanations or any additional text.
+                - If the answer do not provide a response to the question or mentions that available information wasn't sufficient, your response should be the following:  {{"result": [0]}}
+            """)
         }
     }
 }
+JSON_PATTERN = r'{"result": \[[^\]]*\]}'
 
 
 @dataclass
@@ -54,4 +103,46 @@ class Metric(ABC):
     def compute(self, *args, **kwargs) -> float:
         """Needs to be implemented to evaluate a metric"""
         pass
+
+    @staticmethod
+    def extract_response(response: list):
+        """Extracts the json results from a HuggingFace Inference Endpoint response"""
+        eval_json = response[0]['generated_text'].split('\n')[-1]
+
+        try:
+            return np.mean(json.loads(eval_json)['result'])
+        except JSONDecodeError:
+            match = re.search(JSON_PATTERN, eval_json)
+            if match:
+                return np.mean(json.loads(match.group())['result'])
+            else:
+                return eval_json
+
+
+class ContextRecall(Metric):
+    """Assesses how much the answer is based on the context"""
+
+    def compute(self, answer: str, context: str):
+        """Computes context recall given answer and context"""
+        messages = [
+            {'role': 'system', 'content': self.system_prompt},
+            {'role': 'user', 'content': self.user_prompt.format(answer=answer, context=context)}
+        ]
+
+        result = self.llm_provider.query(messages)
+        return self.extract_response(result)
+
+
+class ContextPrecision(Metric):
+    """Assesses how much the context was useful in generating the answer"""
+
+    def compute(self, question: str, answer: str, context: str):
+        """Uses question, answer and context"""
+        messages = [
+            {'role': 'system', 'content': self.system_prompt},
+            {'role': 'user', 'content': self.user_prompt.format(question=question, answer=answer, context=context)}
+        ]
+
+        result = self.llm_provider.query(messages)
+        return self.extract_response(result)
 
