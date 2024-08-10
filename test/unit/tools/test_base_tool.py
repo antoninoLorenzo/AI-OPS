f15960c338756f0febbe6b212af72ee76138d0fc
Antonino Lorenzo <94693967+antoninoLorenzo@users.noreply.github.com>
2024-08-10 13:16:11 +0200

    Implemented load_path test

--- test/unit/tools/test_base_tool.py
+++ test/unit/tools/test_base_tool.py
@@ -10,13 +10,13 @@ class TestBaseTool(unittest.TestCase):
     """Tests the Tool class in `src.agent.tools.base`.
 
     The implemented tests must answer the following questions:
-    1. `load_tool`: create Tool object from json file
+    1. [x] `load_tool`: create Tool object from json file
 
         - what happens if json schema is wrong?
         - what happens if the content of json fields is empty?
         - what happens if the provided path doesn't exist?
 
-    2. `run`: executes a command
+    2. [ ] `run`: executes a command
 
         - what happens if tool execution fails?
     """
