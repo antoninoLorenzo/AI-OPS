3642d807c9aa2398f246fa0899301c7d5da1dd1f
Antonino Lorenzo <94693967+antoninoLorenzo@users.noreply.github.com>
2024-08-11 10:07:51 +0200

    Implemented run test for base tool

--- test/unit/tools/test_base_tool.py
+++ test/unit/tools/test_base_tool.py
@@ -18,9 +18,14 @@ class TestBaseTool(unittest.TestCase):
 
     2. [ ] `run`: executes a command
 
-        - what happens if tool execution fails?
+        - what happens if invalid input is provided? (not the string command)
+        - what happens if command execution fails?
+        - what happens if a command never exits its execution?
+            > some commands will run for long (ex. nmap, hashcat);
+            > inspection should be implemented (...).
     """
     SCHEMA_PATH = Path(Path(__file__).parent / 'base_tool_schemas')
+    CMDS_PATH = Path(Path(__file__).parent / 'cmd_cases.json')
 
     @classmethod
     def setUpClass(cls):
@@ -35,13 +40,25 @@ class TestBaseTool(unittest.TestCase):
             "nested_schema": [{"name": "test"}],
         }
 
+        CMDS_CASES = {
+            "invalid_type": 1,
+            "empty_input": "",
+            "single_arg": "ls",
+            "multi_arg": "ls -la",
+            "wrong_arg": "ls --invalid"
+        }
+
         print(f'--- Creating schema DIR {cls.SCHEMA_PATH}')
         cls.SCHEMA_PATH.mkdir(parents=True, exist_ok=True)
         for name, schema in WRONG_SCHEMAS.items():
-            print(f'--- Creating schema JSON: {name}')
+            # print(f'--- Creating schema JSON: {name}')
             path = str(Path(cls.SCHEMA_PATH / name))
             with open(f'{path}.json', 'w+') as fp:
                 json.dump(schema, fp)
+
+        print(f'--- Creating run test cases {cls.CMDS_PATH}')
+        with open(cls.CMDS_PATH, 'w+') as fp:
+            json.dump(CMDS_CASES, fp)
 
     @classmethod
     def tearDownClass(cls):
@@ -50,11 +67,12 @@ class TestBaseTool(unittest.TestCase):
             for path in cls.SCHEMA_PATH.iterdir():
                 os.remove(str(path))
             os.rmdir(str(cls.SCHEMA_PATH))
+            os.remove(str(cls.CMDS_PATH))
 
-            print(f"Directory {cls.SCHEMA_PATH} has been removed successfully")
+            print(f"Setup files removed successfully")
         except OSError as error:
             print(error)
-            print(f"Directory {cls.SCHEMA_PATH} can not be removed")
+            print(f"Failed to remove one of the following:\n{cls.SCHEMA_PATH}\n{cls.CMDS_PATH}")
 
     def test_load_tool(self):
         print()
@@ -72,6 +90,25 @@ class TestBaseTool(unittest.TestCase):
             expected = EXPECTED[path.name.split('.')[0]]
             print(f'Loading {path.name}; expected: {str(expected)}')
             self.assertRaises(expected, Tool.load_tool, str(path))
+
+    def test_run_tool(self):
+        print()
+        EXPECTED = {
+            "invalid_type": TypeError,
+            "empty_input": ValueError,
+            "single_arg": True,
+            "multi_arg": True,
+            "wrong_arg": True
+        }
+
+        with open(self.CMDS_PATH, 'r') as fp:
+            test_case: dict = json.load(fp)
+            for case, cmd_input in test_case.items():
+                print(f'Executing case {case}: {cmd_input}')
+                if EXPECTED[case] is True:
+                    self.assertIsNotNone(Tool.run(cmd_input), f"Empty output for {cmd_input}")
+                else:
+                    self.assertRaises(EXPECTED[case], Tool.run, cmd_input)
 
 
 if __name__ == "__main__":
