cce0b46bbbe424203c18e529c9aede7ca4ef3b43
Antonino Lorenzo <94693967+antoninoLorenzo@users.noreply.github.com>
2024-08-13 15:39:19 +0200

    Created test_store.py

--- test/unit/store/test_store.py
+++ test/unit/store/test_store.py
@@ -68,6 +68,7 @@ class TestStore(unittest.TestCase):
                 pass
 
     def test_upload(self):
+        # TODO: make collection mocks
         CASES = {
             "type_collection_name":
                 {
