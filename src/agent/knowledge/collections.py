3b6ef8bb1490d4441c094d6c9275062eeebff6c6
Antonino Lorenzo <94693967+antoninoLorenzo@users.noreply.github.com>
2024-08-13 15:37:08 +0200

    Implemented Collection.from_json

--- src/agent/knowledge/collections.py
+++ src/agent/knowledge/collections.py
@@ -1,4 +1,7 @@
 """RAG related data"""
+import os
+import json
+
 from dataclasses import dataclass
 from typing import List, Optional
 
@@ -45,6 +48,78 @@ class Collection:
     topics: List[Topic]
     size: Optional[int] = 0  # points to the number of chunks in a Collection
 
+    @staticmethod
+    def from_json(path: str):
+        """Create a collection from a JSON file in the following format:
+        [
+            {
+                "title": "collection title",
+                "content": "collection content",
+                "category": "collection topics" | ["topic1", "topic2"]
+            },
+            ...,
+        ]
+        :param path: path to the json file.
+        :return: Collection
+        """
+        # path string validation
+        if not path or not isinstance(path, str):
+            raise ValueError("Invalid parameter for path.")
+        elif not os.path.exists(path):
+            raise ValueError(f"Path {path} does not exist.")
+        elif not os.path.isfile(path) or \
+                not os.path.basename(path).endswith(".json"):
+            raise ValueError(f"Path {path} is not JSON file")
+
+        # load json file
+        with open(path, 'r', encoding='utf-8') as fp:
+            data = json.load(fp)
+
+        # json scheme validation
+        format_err_msg = f"Invalid format for {os.path.basename(path)}. "
+        if not isinstance(data, list):
+            raise ValueError(format_err_msg + "Not a list.")
+
+        valid_dict = [isinstance(item, dict) for item in data]
+        if False in valid_dict:
+            raise ValueError(format_err_msg + "Found not dict item.")
+
+        all_keys = [list(item.keys()) for item in data]
+        valid_keys = [
+            'title' in keys and 'content' in keys and 'category' in keys
+            for keys in all_keys
+        ]
+        if False in valid_keys:
+            raise ValueError(format_err_msg + "Not found required keys.")
+
+        documents: list[Document] = []
+        all_topics: list[Topic] = []
+        for item in data:
+            title = item['title']
+            content = item['content']
+            category = item['category']
+            if isinstance(category, list):
+                topics = ', '.join(category)
+                all_topics.extend([Topic(topic) for topic in category])
+            else:
+                topics = category
+                all_topics.append(Topic(category))
+
+            documents.append(Document(
+                name=title,
+                content=content,
+                topic=topics
+            ))
+
+        title = os.path.basename(path).split('.')[0]
+        return Collection(
+            collection_id=-1,
+            title=title,
+            documents=documents,
+            topics=all_topics,
+            size=len(documents)
+        )
+
     def document_names(self) -> list:
         """The document names are used to filter queries to the
         Knowledge Database"""
@@ -57,3 +132,8 @@ class Collection:
         return (f'Title: {self.title} ({self.collection_id})\n'
                 f'| - Topics: {", ".join([topic.name for topic in self.topics])}\n'
                 f'{docs}')
+
+
+if __name__ == "__main__":
+    c = Collection.from_json('../../../data/json/owasp.json')
+    print(c)
