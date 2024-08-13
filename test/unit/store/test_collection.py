1fb05036cd242ad7ef3f7a72745b0862114e083f
Antonino Lorenzo <94693967+antoninoLorenzo@users.noreply.github.com>
2024-08-13 15:39:01 +0200

    Created test_collection.py

---
+++ test/unit/store/test_collection.py
@@ -0,0 +1,18 @@
+import unittest
+
+from src.agent.knowledge.collections import Collection, Document, Topic
+
+
+class CollectionTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # TODO: make collection mocks
+        pass
+
+    def test_from_json(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
