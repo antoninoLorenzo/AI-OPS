09e166ea3efef610e09dc8b2264fc624f267f833
Antonino Lorenzo <94693967+antoninoLorenzo@users.noreply.github.com>
2024-09-11 10:57:23 +0200

    Update setup.py after requirements change

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from setuptools import setup
-
+from pathlib import Path
 from ai_ops_cli import VERSION
 
+CON = {}
+DIR = Path(__file__).parent
+with open(str(DIR / 'requirements-cli.txt'), 'r') as fp:
+    lines = fp.read().splitlines()
+    CON['requirements-cli'] = lines if lines else []
 
 setup(
     name='ai-ops-cli',
     version=VERSION,
     author='@antoninoLorenzo',
-    requires=[
-        'requests', 
-        'rich'
-    ],
+    url='https://github.com/antoninoLorenzo/AI-OPS',
+    install_requires=CON['requirements-cli'],
     py_modules=['ai_ops_cli'],
-    entry_points='''
-        [console_scripts]
-        ai-ops-cli=ai_ops_cli:main
-    ''',
+    entry_points={
+        'console_scripts': ['ai_ops_cli=ai_ops_cli:main']
+    },
 )
