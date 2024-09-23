c284b4e7b27acc2aa8112f74d868628779166163
Antonino Lorenzo <94693967+antoninoLorenzo@users.noreply.github.com>
2024-09-23 11:02:46 +0200

    Updated knowledge init

--- src/__init__.py
+++ src/__init__.py
@@ -5,5 +5,6 @@ def initialize_knowledge(vdb: Store):
     """Used to initialize and keep updated the Knowledge Base.
     Already existing Collections will not be overwritten.
     :param vdb: the reference to the Knowledge Base"""
+    print('[+] Initializing Knowledge with Available Datasets')
     for collection in Store.get_available_datasets():
         vdb.create_collection(collection)
