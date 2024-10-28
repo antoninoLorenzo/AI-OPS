ba37a77be82ea399bed40d2a673b70dc9e89cc1e
Antonino Lorenzo <94693967+antoninoLorenzo@users.noreply.github.com>
2024-10-28 22:31:19 +0100

    Implemented stateful terminal (i.e with session)

--- src/agent/tools/base.py
+++ src/agent/tools/base.py
@@ -1,9 +1,14 @@
 """Tool base class"""
 import json
+import platform
+import queue
 import subprocess
+import threading
+import time
 
 
 class Tool:
+    # TODO: remove, deprecating specific tool integration
     """Represents a Penetration Testing Tool"""
     name: str
     tool_description: str
@@ -53,24 +58,9 @@ class Tool:
 
             return Tool(name, tool_description, args_description, use_case)
 
-    @staticmethod
     def run(*args):
         """Execute a tool"""
-        if not isinstance(args[0], str):
-            raise TypeError(f'Argument must be a string found {type(args[0])}.')
-        if len(args[0]) == 0:
-            raise ValueError('String is empty.')
-
-        command = args[0].encode('utf-8').decode('utf-8')
-        arguments = command.split()
-        result = subprocess.run(arguments, capture_output=True, check=False)
-
-        stdout = result.stdout.decode('utf-8', errors='replace')
-        stderr = result.stderr.decode('utf-8', errors='replace')
-
-        if len(stderr) > 0:
-            return f'{stdout}\n{stderr}'
-        return stdout
+        raise NotImplementedError
 
     def get_documentation(self):
         """Used to provide documentation for the Agent LLM"""
@@ -81,3 +71,83 @@ Description:
 Arguments:
     {self.args_description}          
 """
+
+
+class Terminal:
+    """A stateful terminal implementation"""
+    name: str = 'Terminal'
+    tool_description: str = f'A {platform.system()} Terminal.'
+    args_description: str = f'Takes as argument the string representing the command to execute.'
+    use_case: str = 'Terminal Usage'
+    SHELLS = {
+        'Linux': '/bin/sh',
+        'Windows': 'cmd.exe'  # TODO: change with full path
+    }
+
+    def __init__(self):
+        self.proc = subprocess.Popen(
+            self.SHELLS[platform.system()],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True,
+            text=True
+        )
+        self.reader_output = queue.Queue()
+
+        reader = threading.Thread(target=self.__fetch_output)
+        reader.daemon = True
+        reader.start()
+
+    def run(self, command: str):
+        """Interface to terminal"""
+        if not isinstance(command, str):
+            raise ValueError(f"command should be str, found '{type(command)}'.")
+        self.__exec(command)
+        return ''.join(self.read())
+
+    def __exec(self, command: str):
+        """Write a command to the terminal standard input"""
+        self.proc.stdin.write(f'{command}\n')
+        self.proc.stdin.flush()
+        time.sleep(0.2)
+
+    def __fetch_output(self):
+        """Read the output from terminal standard output"""
+        if self.proc.stdout:
+            for line in iter(self.proc.stdout.readline, ''):
+                self.reader_output.put(line)
+
+            # TODO: handle stderr, not working
+            # for line in iter(proc.stderr.readline, ''):
+            #     stdout.put(line)
+        else:
+            print(f'{self.proc.stdout}')
+
+    def read(self):
+        """A generator that returns single lines from the terminal output"""
+        # wait for output to be added into queue
+        while self.reader_output.empty():
+            pass
+
+        # read from queue
+        while not self.reader_output.empty():
+            yield self.reader_output.get()
+
+    def close(self):
+        """Clean up the subprocess"""
+        if self.proc:
+            self.proc.stdin.close()
+            self.proc.terminate()
+            self.proc.wait()
+
+    def __del__(self):
+        self.close()
+
+
+if __name__ == "__main__":
+    term = Terminal()
+    print(term.run('ls -la'))
+    print(term.run('cd ..'))
+    print(term.run('ls -la'))
+    term.close()
