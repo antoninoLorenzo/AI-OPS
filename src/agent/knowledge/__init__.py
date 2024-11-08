44c435974b32af9f1f0b9b977e0dbde79750d5a8
Antonino Lorenzo <94693967+antoninoLorenzo@users.noreply.github.com>
2024-11-08 18:44:48 +0100

    Merged knowledge.nlp into knowledge.store

--- src/agent/knowledge/__init__.py
+++ src/agent/knowledge/__init__.py
@@ -1,4 +1,3 @@
 """Knowledge Module: contains vector database and documents classes"""
 from src.agent.knowledge.collections import Collection, Document, Topic
-from src.agent.knowledge.nlp import chunk, chunk_str
 from src.agent.knowledge.store import Store
--- src/agent/knowledge/store.py
+++ src/agent/knowledge/store.py
@@ -5,15 +5,18 @@ from typing import Dict, Optional
 
 import httpx
 import ollama
+import spacy
 import pandas as pd
 import qdrant_client.http.exceptions
 from qdrant_client import QdrantClient, models
 from qdrant_client.http.exceptions import UnexpectedResponse
 from tqdm import tqdm
 
-from src.agent.knowledge.collections import Collection, Document, Topic
-from src.agent.knowledge.nlp import chunk
 from src.agent.llm.llm import ProviderError
+from src.agent.knowledge.collections import Collection, Document, Topic
+
+
+nlp = spacy.load("en_core_web_md")
 
 
 class Store:
@@ -289,3 +292,40 @@ class Store:
         if name not in self.collections:
             return None
         return self._collections[name]
+
+
+def chunk(document: Document):
+    """Return chunks of a Document that will be added to the Vector Database"""
+    return chunk_str(document.content)
+
+
+def chunk_str(document: str):
+    """Chunks a text string, the chunking strategy is:
+    NLP sentence extraction -> sentence grouping by similarity.
+    """
+    doc = nlp(document)
+    sentences = [
+        sent for sent in list(doc.sents)
+        if str(sent).strip() not in ['*']
+    ]
+
+    similarities = []
+    for i in range(1, len(sentences)):
+        if not sentences[i - 1].has_vector or not sentences[i].has_vector:
+            similarities.append(0.0)
+        else:
+            sim = sentences[i-1].similarity(sentences[i])
+            similarities.append(sim)
+
+    threshold = 0.5
+    max_sent = 4
+    sentences = [str(sent) for sent in sentences]
+    groups = [[sentences[0]]]
+    for i in range(1, len(sentences)):
+        if len(groups[-1]) > max_sent or similarities[i-1] < threshold:
+            groups.append([sentences[i]])
+        else:
+            groups[-1].append(sentences[i])
+
+    _chunks = [" ".join(g) for g in groups]
+    return [_ch for _ch in _chunks if len(_ch) > 100]
